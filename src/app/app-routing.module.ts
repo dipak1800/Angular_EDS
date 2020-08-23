3355c817aa9fc3a5a10c06b9f58d33d02b8d11fa
Dipak Roy <59052983+dipak1800@users.noreply.github.com>
2020-08-24 02:15:05 +0530

    Added routing for homepage and employ details page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
+import { AppSideNavComponent } from './app-side-nav/app-side-nav.component';
+import { EmployPageComponent } from './employ-page/employ-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppSideNavComponent,
+  },
+  {
+    path: 'employees',
+    component: EmployPageComponent,
+  },
+  // {
+  //   path: 'employees:id',
+  //   component: EmployPageComponent,
+  // },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+export const routingComponents = [EmployPageComponent, AppSideNavComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,20 @@
 import { MaterialModule } from './material/material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppSideNavComponent } from './app-side-nav/app-side-nav.component';
+// import { AppSideNavComponent } from './app-side-nav/app-side-nav.component';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { EmployPageComponent } from './employ-page/employ-page.component';
+// import { EmployPageComponent } from './employ-page/employ-page.component';
 import { EmployDetailsPageComponent } from './employ-details-page/employ-details-page.component';
 @NgModule({
   declarations: [
     AppComponent,
-    AppSideNavComponent,
     LoginComponent,
-    EmployPageComponent,
+    routingComponents,
     EmployDetailsPageComponent,
   ],
   imports: [
